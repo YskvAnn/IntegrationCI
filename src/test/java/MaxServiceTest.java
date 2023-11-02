811a8a66cbff961bdfb87c251d52b4fb59e13588
AnnNetoTest <yaskovaann2002@mail.ru>
2023-11-02 13:59:53 +0300

    initiall commit

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -11,9 +11,23 @@ public class MaxServiceTest {
         int a = 5;
         int b = 3;
 
-        int expected = 5;
+        int expected = a;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals (expected, actual);
+    }
+    @Test
+
+    public void ShouldFindMaxIfB(){
+        MaxService service = new MaxService();
+
+        int a = 3;
+        int b = 5;
+
+        int expected = b;
         int actual = service.max(a, b);
 
         Assertions.assertEquals (expected, actual);
     }
     }
+
